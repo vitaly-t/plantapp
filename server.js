daf4c721e5d2db583313f6841ad755c1962cff38
Scott Hill <scott289@gmail.com>
2016-08-16 09:59:11 -0700

    blah

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
 
+var mongoose = require('mongoose');
+
+
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
@@ -10,6 +13,23 @@ new WebpackDevServer(webpack(config), {
   if (err) {
     console.log(err);
   }
+
+
+
+// connect to Mongo when the app initializes
+  mongoose.connect('mongodb://localhost/plantsdb', function (error) {
+    if (error) {
+      console.log(error);
+    }
+  });
+
+
+// Enable api routes
+  const api_routes = require('./api_routes');
+  app.use('/api', api_routes);
+
+
+
 
   console.log('Listening at localhost:3000');
 });
