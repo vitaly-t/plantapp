3d53b6f955cd0cf1b376707cced3724201904628
Scott Hill <scott289@gmail.com>
2016-09-22 10:48:42 -0700

    port clarity

--- server/index.js
+++ server/index.js
@@ -4,15 +4,20 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const app = express();
 
-app.set('port', (process.env.API_PORT || 3001));
 app.use(bodyParser.json());
 app.use(morgan("dev"));
 
 console.log("Environ:", process.env.NODE_ENV);
+console.log("process.env.SERVER_PORT", process.env.SERVER_PORT);
+console.log("process.env.CLIENT_PORT", process.env.CLIENT_PORT);
+console.log("process.env.PORT", process.env.PORT);
 
-// If this is prod, serve the client build dir
+// If this is prod, serve the client build dir and set port
 if (process.env.NODE_ENV === "production") {
+  app.set('port', (process.env.PORT));
   app.use(express.static(path.join(__dirname, '../client/build')));
+} else {
+  app.set('port', (process.env.SERVER_PORT || 3001));
 }
 
 // Enable routes with /api prefix
